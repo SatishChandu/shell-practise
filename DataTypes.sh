1873d590d2b3e415d12444a4f8318554eff4079c
SatishKumarChandu <satish.chandu@cmsedge.com>
2026-05-26 00:10:22 +0530

    Commit for practise

---
+++ DataTypes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=250
+
+TIMESTAMP=$(date)
+
+echo "The time of execution is: $TIMESTAMP"
+
+SUM=$((NUMBER1 + NUMBER2))
+
+echo "The sum of $NUMBER1 and $NUMBER2 is: $SUM"
