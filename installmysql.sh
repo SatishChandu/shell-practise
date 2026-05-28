f7a7ec52511c130ddddfc829f279b5108f433e47
SatishKumarChandu <satish.chandu@cmsedge.com>
2026-05-29 00:04:57 +0530

    Commit for practise

--- installmysql.sh
+++ installmysql.sh
@@ -10,12 +10,22 @@ else
     echo "You are running with root access."
 fi
 
-dnf install mysql-server -y
+dnf list installed mysql
 
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
-    echo "MySQL server installed successfully."
+    echo "MySQL server is not installed. Installing now..."
+    dnf install mysql-server -y
+
+    if [ $? -eq 0 ]
+    then
+        echo "MySQL server installed successfully."
+    else
+        echo "Error: Failed to install MySQL server."
+        exit 1
+    fi
 else
-    echo "Error: Failed to install MySQL server."
-    exit 1
+    echo "MySQL server is already installed."
 fi
+
+
